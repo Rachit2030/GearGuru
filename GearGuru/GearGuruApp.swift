d71b50756f801d86d0093138e41fba4cb5eb702f
Rachit2030 <goyalrachit2030@gmail.com>
2024-05-09 15:33:02 +0530

    Initial Commit

---
+++ GearGuru/GearGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  GearGuruApp.swift
+//  GearGuru
+//
+//  Created by Rachit on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GearGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
